e04213b6b8ea4032ff93c4fc8692f384ed605cf0
juniorrondini <jjrondinijj@gmail.com>
2024-01-31 08:43:14 -0300

    first endpoint finished

--- src/main/java/com/sinister/api/controller/ProtectionClientController.java
+++ src/main/java/com/sinister/api/controller/ProtectionClientController.java
@@ -5,6 +5,7 @@ import com.sinister.api.service.ProtectionClientService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,9 +20,29 @@ public class ProtectionClientController {
     public ResponseEntity<?> get(){
         return ResponseEntity.ok(protectionClientService.get());
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id){
+        return ResponseEntity.ok(protectionClientService.getById(id));
+    }
+
+
     @PostMapping
     public ResponseEntity<?> post(@RequestBody ProtectionClients protectionClient){
         return  ResponseEntity.ok(protectionClientService.save(protectionClient));
+
+        }
+    @DeleteMapping("/{id}")
+    public ResponseEntity <?> delete(@PathVariable Long id) {
+        protectionClientService.delete(id);
+        return ResponseEntity.noContent().build();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable Long id, @RequestBody ProtectionClients updatedClient){
+        protectionClientService.put(id, updatedClient);
+        return ResponseEntity.ok().build();
+    }
+
+
 }
+
 
--- src/main/java/com/sinister/api/service/ProtectionClientService.java
+++ src/main/java/com/sinister/api/service/ProtectionClientService.java
@@ -8,18 +8,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProtectionClientService {
     @Autowired
-    private  ProtectionClientRepository protectionClientRepository;
-    public ProtectionClients save(ProtectionClients protectionClients){
+    private ProtectionClientRepository protectionClientRepository;
+
+    public ProtectionClients save(ProtectionClients protectionClients) {
         return protectionClientRepository.save(protectionClients);
     }
 
-    public List<ProtectionClients> get(){
+    public List<ProtectionClients> get() {
         return protectionClientRepository.findAll();
     }
-}
+
+    public Optional<ProtectionClients> getById(Long id) {
+        return protectionClientRepository.findById(id);
+    }
+
+    public Optional<ProtectionClients> put(Long id, ProtectionClients updatedClient) {
+        return protectionClientRepository.findById(id).map(existingClient -> {
+            existingClient.setTitle(updatedClient.getTitle());
+            existingClient.setAddress(updatedClient.getAddress());
+            existingClient.setPayCheck(updatedClient.getPayCheck());
+            return protectionClientRepository.save(existingClient);
+        });
+    }
+
+        public void delete (Long id){
+            protectionClientRepository.deleteById(id);
+        }
+
+    }
+
+
+
